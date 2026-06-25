a280b76f1d37e9034b4c4f11411fb1dcb764bb98
admiralmanan <fikricrb001@gmail.com>
2026-06-26 04:28:26 +0700

    Add JUnit test for Movie and Continues Testing Workflow

---
+++ AplikasiStreaming/src/test/java/com/mycompany/aplikasistreaming/film/MovieJUnitTest.java
@@ -0,0 +1,24 @@
+package com.mycompany.aplikasistreaming.film;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MovieJUnitTest {
+
+    @Test
+    void testMovieTitle() {
+
+        Movie movie = new Movie(
+                "M001",
+                "Avengers",
+                "Action",
+                2019,
+                181,
+                8.4,
+                "url",
+                true
+        );
+
+        assertEquals("Avengers", movie.getTitle());
+    }
+}
